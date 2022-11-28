2273336e357c02d4e9ba49cec22821349a3ae298
Anne Staff <anne.staff@pm.me>
2022-11-28 16:32:11 +0100

    fixes

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from maze import Maze
 
 
 if __name__ == '__main__':
-    print("Use the arrow keys to move along the maze.")
+    print("Use the arrow keys to move along the maze. Press ESC to stop the game.")
     new_game = Game((20, 20))
     new_game.play()
